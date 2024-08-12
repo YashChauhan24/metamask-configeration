bab3e14fa1266037dca8665edd5bfa53610ca4e9
YashChauhan24 <chauhanyash2412@gmail.com>
2024-08-13 00:20:31 +0530

    metamask conection and dispay of account, balance, network id done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Web3 from "web3";
 
 function App() {
+  const [account, setAccount] = useState("");
+  const [balance, setBalance] = useState("");
+  const [network, setNetwork] = useState("");
+
+  const checkMetaMaskConnection = async () => {
+    if (window.ethereum) {
+      try {
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        const web3 = new Web3(window.ethereum);
+        const accounts = await web3.eth.getAccounts();
+        setAccount(accounts[0]);
+        const bal = await web3.eth.getBalance(accounts[0]);
+        const ethBalance = web3.utils.fromWei(bal, "ether");
+        setBalance(ethBalance);
+        const networkId = await web3.eth.net.getId();
+        setNetwork(networkId.toString());
+      } catch (err) {
+        console.error("Error connecting to MetaMask", err);
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h3> wallet address: {account}</h3>
+      <h3> wallet balance: {balance}</h3>
+      <h3>network Id: {network}</h3>
+      <button type="button" onClick={() => checkMetaMaskConnection()}>
+        Connect MetaMask
+      </button>
     </div>
   );
 }
